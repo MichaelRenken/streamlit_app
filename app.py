0758d08a14538829f9252b14719105ffdb3722bc
MichaelRenken <126526439+MichaelRenken@users.noreply.github.com>
2023-04-27 14:30:36 -0500

    google cloud debug 1

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ client = storage.Client(credentials=credentials)
 @st.cache_data()
 def read_file(bucket_name, file_path):
     bucket = client.bucket(bucket_name)
-    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    content = bucket.blob(file_path)
     return content
 
 bucket_name = "mr_streamlit_app"
