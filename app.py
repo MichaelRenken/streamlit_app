bfbdfd51a542c442b7136b3bce08fc71a07f3b8e
MichaelRenken <126526439+MichaelRenken@users.noreply.github.com>
2023-04-26 21:57:02 -0500

    stopwords fix 2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 ###Model Code###
 import joblib
 import nltk #importing a stop words function from nltk
-nltk.download('stopwords')
+nltk.download('stopwords.words')
 import string #importing a package of usefull strings. Will use string.punctuation for this
 
 stemmer = nltk.stem.PorterStemmer()
