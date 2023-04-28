e4354349adeb43cc58e7000efcdb026953e574da
MichaelRenken <126526439+MichaelRenken@users.noreply.github.com>
2023-04-27 21:41:01 -0500

    markdown and dataframe output

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-#from sqlite3 import Blob
+#imports
 import streamlit as st
-
-###Model Code###
 import joblib
 import nltk #importing a stop words function from nltk
 import string #importing a package of usefull strings. Will use string.punctuation for this
 from google.oauth2 import service_account
 from google.cloud import storage
 import io
+import pandas as pd
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
@@ -18,13 +17,13 @@ client = storage.Client(credentials=credentials)
 bucket_name = "mr_streamlit_app"
 file_path = "BR_streamlit.sav"
 
-# Retrieve file contents.
+# Retrieve BR model contents.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_resource()
 def read_file(bucket_name, file_path):
-    bucket = client.get_bucket(bucket_name)
-    content = bucket.blob(file_path)
-    #create the model object to run predictins from
+    bucket = client.get_bucket(bucket_name) #find gcs bucket
+    content = bucket.blob(file_path) #get model from bucket as blob
+    #turn model object into bytes to joblib back into correct form
     model_bytes = io.BytesIO() #create bytes object
     content.download_to_file(model_bytes)  #transfer the sav to the bytes object
     model_bytes.seek(0) #changes the position of the pointer
@@ -33,187 +32,12 @@ def read_file(bucket_name, file_path):
     
 model = read_file(bucket_name,file_path)
 
+#define the tokenizer for the TFIDF
 def my_tokenizer(sentence):
     stemmer = nltk.stem.PorterStemmer()
-    st_words = ['i',
-    'me',
-    'my',
-    'myself',
-    'we',
-    'our',
-    'ours',
-    'ourselves',
-    'you',
-    "you're",
-    "you've",
-    "you'll",
-    "you'd",
-    'your',
-    'yours',
-    'yourself',
-    'yourselves',
-    'he',
-    'him',
-    'his',
-    'himself',
-    'she',
-    "she's",
-    'her',
-    'hers',
-    'herself',
-    'it',
-    "it's",
-    'its',
-    'itself',
-    'they',
-    'them',
-    'their',
-    'theirs',
-    'themselves',
-    'what',
-    'which',
-    'who',
-    'whom',
-    'this',
-    'that',
-    "that'll",
-    'these',
-    'those',
-    'am',
-    'is',
-    'are',
-    'was',
-    'were',
-    'be',
-    'been',
-    'being',
-    'have',
-    'has',
-    'had',
-    'having',
-    'do',
-    'does',
-    'did',
-    'doing',
-    'a',
-    'an',
-    'the',
-    'and',
-    'but',
-    'if',
-    'or',
-    'because',
-    'as',
-    'until',
-    'while',
-    'of',
-    'at',
-    'by',
-    'for',
-    'with',
-    'about',
-    'against',
-    'between',
-    'into',
-    'through',
-    'during',
-    'before',
-    'after',
-    'above',
-    'below',
-    'to',
-    'from',
-    'up',
-    'down',
-    'in',
-    'out',
-    'on',
-    'off',
-    'over',
-    'under',
-    'again',
-    'further',
-    'then',
-    'once',
-    'here',
-    'there',
-    'when',
-    'where',
-    'why',
-    'how',
-    'all',
-    'any',
-    'both',
-    'each',
-    'few',
-    'more',
-    'most',
-    'other',
-    'some',
-    'such',
-    'no',
-    'nor',
-    'not',
-    'only',
-    'own',
-    'same',
-    'so',
-    'than',
-    'too',
-    'very',
-    's',
-    't',
-    'can',
-    'will',
-    'just',
-    'don',
-    "don't",
-    'should',
-    "should've",
-    'now',
-    'd',
-    'll',
-    'm',
-    'o',
-    're',
-    've',
-    'y',
-    'ain',
-    'aren',
-    "aren't",
-    'couldn',
-    "couldn't",
-    'didn',
-    "didn't",
-    'doesn',
-    "doesn't",
-    'hadn',
-    "hadn't",
-    'hasn',
-    "hasn't",
-    'haven',
-    "haven't",
-    'isn',
-    "isn't",
-    'ma',
-    'mightn',
-    "mightn't",
-    'mustn',
-    "mustn't",
-    'needn',
-    "needn't",
-    'shan',
-    "shan't",
-    'shouldn',
-    "shouldn't",
-    'wasn',
-    "wasn't",
-    'weren',
-    "weren't",
-    'won',
-    "won't",
-    'wouldn',
-    "wouldn't"]
+    #manually added stopwords as nltk was not working with streamlit
+    st_words = ['i','me','my','myself','we','our','ours','ourselves','you',"you're","you've","you'll","you'd",'your','yours','yourself','yourselves','he','him','his','himself','she',"she's",'her','hers','herself','it',"it's",'its','itself', 'they', 'them', 'their','theirs','themselves','what','which','who','whom','this','that',"that'll",'these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don',"don't",'should',"should've",'now','d','ll','m','o','re','ve','y','ain','aren',"aren't",'couldn',"couldn't",'didn',"didn't",'doesn',"doesn't",'hadn',"hadn't",'hasn',"hasn't",'haven',"haven't",'isn',"isn't",'ma','mightn',"mightn't",'mustn',"mustn't",'needn',"needn't",'shan',"shan't",'shouldn',"shouldn't",'wasn',"wasn't",'weren',"weren't",'won',"won't",'wouldn',"wouldn't"]
+    
     #remove newline characters (Source:dylancastillo.co)
     sentence = " ".join(sentence.split())
 
@@ -233,27 +57,26 @@ def my_tokenizer(sentence):
             listofstemmed_words.append(stemmed_word)
     return listofstemmed_words
 
-
-#model = joblib.load('mlknn_streamlit.sav') #model file
-
+#pull the TFIDF and genrelist from github
 TFIDF = joblib.load('TFIDF.sav') #tokenizer file
 genrelist = joblib.load('genrelist.sav') #genre name file
 
-
+#-------------------APP CODE---------------------------------------
 #Header
 st.title("This is Michael Renken's Book Description Genre Classifier!!")
 ###ask for input###
-with st.form(key='my_form'):
+with st.form(key='my_form'): #connect text input to button
     input_string = st.text_area("Input a book description here (feel free to write your own)",value='“Once upon a time, a very long time ago now, about last Friday, Winnie-the-Pooh lived in a forest all by himself under the name of Sanders.” Curl up with a true children’s classic by reading A.A.Milne’s Winnie-the-Pooh with iconic decorations by E.H.Shepard. Winnie-the-Pooh may be a bear of very little brain, but thanks to his friends Piglet, Eeyore and, of course, Christopher Robin, he’s never far from an adventure. In this much-loved classic story collection Pooh gets into a tight place, nearly catches a Woozle and heads off on an ‘expotition’ to the North Pole with the other animals.')
-    submit_button = st.form_submit_button(label='Submit')
+    submit_button = st.form_submit_button(label='Submit') #submit button so that moble users can use (otherwise have to hit ctrl+enter)
 ###run the model###
-test = TFIDF.transform([input_string])
-
-output_test = model.predict(test).nonzero()[1]
+test = TFIDF.transform([input_string]) #run TFIDF on input
+output_test = model.predict_proba(test)[0] #Get model probabilities
 #translate these predictions from the genre indexes to the genre titles
-answerlist = []
-for i in output_test:
-    answerlist.append(list(genrelist.keys())[i])
+final_output = pd.DataFrame(columns=['Confidence']) #create empty dataframe to store outputs
+for i,v in enumerate(output_test.tolist()): #input all probabilities into dataframe >50% probability
+    if v >.5:
+        final_output.loc[list(genrelist.keys())[i]] = [v] #replace the genre # with a value and enter into dataframe
+final_output.sort_values(by='Confidence',ascending=False,inplace=True) #sort dataframe from most to least likely
 
-st.write('The model predicts that this book belongs to the below genres (ordered from most common genres in the training set):')
-st.write(answerlist)
+st.text('The model predicts that this book belongs to the below genres:') #text above output
+st.dataframe(final_output) #output final dataframe
